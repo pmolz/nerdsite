d5472e11ef8de64c6c562ad46d410d9e52a05780
Peter Molzer <pete.molzer@gmail.com>
2023-02-16 18:27:29 -0500

    3 ball

--- html/bouncy/main.js
+++ html/bouncy/main.js
@@ -69,7 +69,7 @@ class Ball {
 }
 const balls = [];
 
-while (balls.length < 25) {
+while (balls.length < 3) {
   const size = random(10, 20);
   const ball = new Ball(
     // ball position always drawn at least one ball width
