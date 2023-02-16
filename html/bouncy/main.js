47bc34e1e28988851eabccd6f7728265946038d8
Peter Molzer <pete.molzer@gmail.com>
2023-02-16 13:29:00 -0500

    fix

--- html/bouncy/main.js
+++ html/bouncy/main.js
@@ -1,5 +1,5 @@
 //setup canvas
-
+import Math;
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
 
@@ -9,7 +9,7 @@ const height = canvas.height = window.innerHeight;
 //function to generate random number
 
 function random(min, max) {
-  const num = Math.floor(Math.random()  (max - min + 1)) + min;
+  const num = Math.floor(Math.random() * (max - min + 1)) + min;
   return num;
 }
 
